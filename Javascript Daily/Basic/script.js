5184a0da9ee018d0df5c7aafd957d365b057e5e5
akshar51 <aksharparekh401@gmail.com>
2025-01-27 18:01:38 +0530

    done

--- Javascript Daily/Basic/script.js
+++ Javascript Daily/Basic/script.js
@@ -9,8 +9,26 @@
 
 
 // FUNCTION IN ES6
-let add = (a,b)=>{
-    return a+b;
+// let add = (a,b)=>{
+//     return a+b;
+// }
+
+// console.log(add(50,10));
+
+//TEMPLATE LITERAL
+// let name="Hello";
+// console.log(`Hii ,${name}`);
+
+
+//CLASSES
+class Person{
+    constructor(name){
+        this.name=name;
+    }
+    greet(){
+        console.log(`Hello,${this.name}`);
+    }
 }
 
-console.log(add(50,10));
+let p1 = new Person("Rahul");
+p1.greet();
