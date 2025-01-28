3214d827229f688c3ce822d85f82ccf7a7b2853e
akshar51 <aksharparekh401@gmail.com>
2025-01-28 17:55:00 +0530

    done

--- Javascript Daily/Output/script.js
+++ Javascript Daily/Output/script.js
@@ -14,7 +14,20 @@
 // content.textContent="ECMA SCRIPT...";
 
 // PROMPT
-
 // let name1=prompt("Enter your name : ");
 // console.log(name1);
 
+// TEMPLATE LITERALS
+// let a=10;
+// let b=20;
+// console.log(`Sum of a and b is : ${a+b}`);
+
+// APPENDING TO DOM (ADDING ELEMENT TO HTML)
+// let head = document.createElement("h1");
+// head.textContent = "Javascript";
+// document.body.appendChild(head);
+
+// DISPLAY TABLE IN CONSOLE
+// let table = [{name:"Akshar",age:23},{name:"Salman",age:55}];
+// console.table(table);
+// console.log(table[1]);
