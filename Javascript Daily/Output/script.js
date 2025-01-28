438c2437662e2d0d8fe05ca0527ff6224e728dc7
akshar51 <aksharparekh401@gmail.com>
2025-01-28 17:33:09 +0530

    done

--- Javascript Daily/Output/script.js
+++ Javascript Daily/Output/script.js
@@ -2,4 +2,11 @@
 //document.write("Hello Js...");
 
 // ALERT (POP-UP)
-alert("Error of alert..!!")
+//alert("Error of alert..!!")
+
+// INSERT CONTENT INTO HTML 
+let title=document.getElementById("heading");
+title.innerHTML="Hello Jscpt....<p>Lorem</p>";
+console.log(title);
+
+// INSERT CONTENT INTO HTML
