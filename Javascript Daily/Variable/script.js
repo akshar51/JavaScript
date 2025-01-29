104ce26c5ea71edf898d349954c6261fbaf1cacc
akshar51 <aksharparekh401@gmail.com>
2025-01-29 17:47:03 +0530

    done

--- Javascript Daily/Variable/script.js
+++ Javascript Daily/Variable/script.js
@@ -32,11 +32,11 @@
 // OPERATOR
 // 1) ARITHMETIC OPERATOR
 
-let a=10;
-let b=2;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
-console.log(a**3);// EXPONENTIAL-means power
+// let a=10;
+// let b=2;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**3);// EXPONENTIAL-means power
