e928a6dbc9663ae639cbc97c77c08f346b4b5c5e
akshar51 <aksharparekh401@gmail.com>
2025-01-29 17:32:33 +0530

    done

--- Javascript Daily/Variable/script.js
+++ Javascript Daily/Variable/script.js
@@ -4,13 +4,13 @@
 // console.log(age)
 
 // let 
-let name = "akshar";
-console.log(name);
+// let name = "akshar";
+// console.log(name);
 
 // const
-const age=25;
-age=30;
-console.log(age)
+// const age=25;
+// age=30;
+// console.log(age)
 
 // DATA TYPE
 // 1) NULL
@@ -20,3 +20,12 @@ console.log(age)
 // 2)Undefined
 // let studentName;
 // console.log(studentName);
+
+// Object
+
+let obj={
+    student:"aks",
+    age:33,
+    
+}
+console.log(obj);
