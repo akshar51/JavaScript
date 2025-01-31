95842bc976296bf2eaf0ba62f1f28c8ef2ac8c55
akshar51 <aksharparekh401@gmail.com>
2025-01-31 12:51:11 +0530

    done

--- Practice/script.js
+++ Practice/script.js
@@ -1,13 +1,31 @@
 var isstatus=document.querySelector("h1");
-var btn=document.querySelector(".btn-cont button");
-var removefriend=document.querySelector("#remove");
+var btn = document.querySelector("#add");
 
+// var removefriend=document.querySelector("#remove");
+
+// TWO BUTTON 
+// btn.addEventListener("click",function(){
+//     isstatus.innerHTML="Friend";
+//     isstatus.style.color="green";
+// })
+
+// removefriend.addEventListener("click",function(){
+//     isstatus.innerHTML="Stranger";
+//     isstatus.style.color="red"
+// })
+
+// SINGLE BUTTON
+var check=0
 btn.addEventListener("click",function(){
-    isstatus.innerHTML="Friend";
-    isstatus.style.color="green";
-})
-
-removefriend.addEventListener("click",function(){
-    isstatus.innerHTML="Stranger";
-    isstatus.style.color="red"
+    if(check == 0){
+        isstatus.innerHTML="Friend";
+        isstatus.style.color="green";
+        console.log("hiii")
+        check=1;
+    }
+    else{
+        isstatus.innerHTML="Stranger";
+        isstatus.style.color="red" ;
+        check=0;
+    }
 })
