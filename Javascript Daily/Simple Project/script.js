1fefb4862f69de290e0a631ce821cb9781bb09c3
akshar51 <aksharparekh401@gmail.com>
2025-01-31 17:22:37 +0530

    done

---
+++ Javascript Daily/Simple Project/script.js
@@ -0,0 +1,18 @@
+// GRADE
+//  let avg=prompt("Enter your grade : ");
+
+//  if(avg>=90){
+//     console.log("Grade A");
+//  }
+//  else if(avg>=80){
+//     console.log("Grade B");
+//  }
+//  else if(avg>=70){
+//     console.log("Grade C");
+//  }
+//  else if(avg>=45){
+//     console.log("Grade D");
+//  }
+//  else{
+//     console.log("Pass");
+//  }
