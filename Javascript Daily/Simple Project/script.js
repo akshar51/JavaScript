65c49a6dd9399ffb502a18dad4ef074e6913ee01
akshar51 <aksharparekh401@gmail.com>
2025-01-31 17:29:42 +0530

    done

--- Javascript Daily/Simple Project/script.js
+++ Javascript Daily/Simple Project/script.js
@@ -16,3 +16,17 @@
 //  else{
 //     console.log("Pass");
 //  }
+
+// BILL
+let p_name="Oil";
+let p_qty=2;
+let cost=160;
+
+let discount=0.1;
+
+let total=p_qty*cost;
+let net=total-discount;
+console.log("Quantity : ",p_qty);
+console.log("Cost :",cost);
+console.log("Total : ",total);
+console.log("Total after discount : ",net);
