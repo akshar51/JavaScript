841c0a29a6826bd205425860dfd8648fde79ae69
akshar51 <aksharparekh401@gmail.com>
2025-02-03 16:58:30 +0530

    done

---
+++ Javascript Daily/if-else/script.js
@@ -0,0 +1,51 @@
+// 1) NUMBER IS POSITIVE
+// let num=7;
+// if(num>0){
+//     console.log(` ${num} is positive..`);
+// }
+
+// 2) NUMBER IS EVEN OR ODD
+// let num=5;
+// if(num%2===0){
+//     console.log(`${num} is even..`)
+// }
+// else{
+//     console.log(`${num} is odd..`)
+// }
+
+// 3) DIVISIBLE BY 3 & 5
+// let num=25;
+// if(num%3==0){
+//     if(num%5==0){
+//         console.log(`${num} is divisible by both 3 and 5.`)
+//     }
+//     else{
+//         console.log(`${num} is divisible by 3 but not by 5.`)
+//     }
+// }
+// else{
+//     console.log(`${num} is not divisible by 3.`)
+// }
+
+// 4) GRADE BASED ON SCORE
+// let score=85;
+// if(score>=90){
+//     console.log(`Grade : A`)
+// }
+// else if(score>=80){
+//     console.log(`Grade : B`)
+// }
+// else if(score>=70){
+//     console.log(`Grade : C`)
+// }
+// else if(score>=60){
+//     console.log(`Grade : D`)
+// }
+// else{
+//     console.log(`Grade : F`)
+// }
+
+
+
+
+
