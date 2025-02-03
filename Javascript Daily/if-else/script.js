937d3ee01abcdaa499c9d5c69cf5016cc542edf6
akshar51 <aksharparekh401@gmail.com>
2025-02-03 17:21:08 +0530

    done

--- Javascript Daily/if-else/script.js
+++ Javascript Daily/if-else/script.js
@@ -75,37 +75,52 @@
 //  }
 
 // 7) DAY OF WEEK 
-let dayNumber=4;
-let dayName=""
+// let dayNumber=4;
+// let dayName=""
 
-if(dayNumber===1){
-    dayName = "Monday"
-}
-else if(dayNumber===2){
-    dayName = "Tuesday"
-}
-else if(dayNumber===3){
-    dayName = "Wednesday"
-}
-else if(dayNumber===4){
-    dayName = "Thursday"
-}
-else if(dayNumber===5){
-    dayName = "Friday"
-}
-else if(dayNumber===6){
-    dayName = "Saturday"
-}
-else if(dayNumber===7){
-    dayName = "Sunday"
+// if(dayNumber===1){
+//     dayName = "Monday"
+// }
+// else if(dayNumber===2){
+//     dayName = "Tuesday"
+// }
+// else if(dayNumber===3){
+//     dayName = "Wednesday"
+// }
+// else if(dayNumber===4){
+//     dayName = "Thursday"
+// }
+// else if(dayNumber===5){
+//     dayName = "Friday"
+// }
+// else if(dayNumber===6){
+//     dayName = "Saturday"
+// }
+// else if(dayNumber===7){
+//     dayName = "Sunday"
+// }
+// else{
+//     dayName = "Invalid day"
+// }
+
+// console.log(`Day ${dayNumber} is ${dayName}.`)
+
+// 8) DISCOUNT ELIGIBILITY
+
+let isMember = true;
+let spending = 150;
+
+if(isMember){
+    if(spending > 100){
+        console.log(`You get a 20% discount.`)
+    }
+    else{
+        console.log(`You get a 10% discount.`)
+    }
 }
 else{
-    dayName = "Invalid day"
+    console.log(`No discount available for non-member`)
 }
-
-console.log(`Day ${dayNumber} is ${dayName}.`)
-
-
 
 
 
