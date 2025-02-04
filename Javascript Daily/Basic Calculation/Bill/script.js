06c103bc537c8074400e8f996df64a80f2883537
akshar51 <aksharparekh401@gmail.com>
2025-02-04 18:01:28 +0530

    done

---
+++ Javascript Daily/Basic Calculation/Bill/script.js
@@ -0,0 +1,8 @@
+let name = "Ghee";
+let price = 800;
+let qty = 1;
+total=price*qty;
+
+let netTotal = (price>=1500)?total=total-15% : price>=1000 ? total=total-10% : price>=800 ? total=total-8% : total;
+
+
---
+++ Javascript Daily/Basic Calculation/Grade/script.js
@@ -0,0 +1,4 @@
+let avg = prompt("Enter your number")
+let grade = avg>=80?"A" : avg>=60?"B": avg>=50?"C" : "Pass exam"
+console.log(`Grade is ${grade}`)
+
