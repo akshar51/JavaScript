75d611bdf0633c8f1d199a5424445d5cc9701b06
akshar51 <aksharparekh401@gmail.com>
2025-02-04 18:07:00 +0530

    done

--- Javascript Daily/Basic Calculation/Bill/script.js
+++ Javascript Daily/Basic Calculation/Bill/script.js
@@ -3,6 +3,5 @@ let price = 800;
 let qty = 1;
 total=price*qty;
 
-let netTotal = (price>=1500)?total=total-15% : price>=1000 ? total=total-10% : price>=800 ? total=total-8% : total;
-
+let netTotal = total>=1500?total=total-15% : total>=1000 ? total=total-10% : total>=800 ? total=total-8% : total
 
