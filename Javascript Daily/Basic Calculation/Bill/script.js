92868a44e7790513c960681163614d35df81e9ad
akshar51 <aksharparekh401@gmail.com>
2025-02-04 23:22:06 +0530

    done

--- Javascript Daily/Basic Calculation/Bill/script.js
+++ Javascript Daily/Basic Calculation/Bill/script.js
@@ -1,7 +1,18 @@
 let name = "Ghee";
 let price = 800;
-let qty = 1;
-total=price*qty;
+let qty = 2;
+let total=price*qty;
+let discount1=0.15;
+let discount2=0.1;
+let discount3=0.08;
 
-let netTotal = total>=1500?total=total-15% : total>=1000 ? total=total-10% : total>=800 ? total=total-8% : total
+let netTotal = total>=1500?nettotal=total-(total*discount1) : 
+            total>=1000?nettotal=total-(total*discount2) : 
+            total>=800?nettotal=total-(total*discount3) : 
+            total
 
+console.log(`Product is : ${name}`);
+console.log(`Product price is : ${price}`);
+console.log(`Quantity is :${qty}`);
+console.log(`Total is : ${total}`);
+console.log(`Net total after discount : ${netTotal}`);
