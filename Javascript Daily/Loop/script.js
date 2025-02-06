5e0474802b18ddfcdb4ea51f95ca00b5af8e7764
akshar51 <aksharparekh401@gmail.com>
2025-02-06 17:34:15 +0530

    done

--- Javascript Daily/Loop/script.js
+++ Javascript Daily/Loop/script.js
@@ -1,15 +1,26 @@
 // FOR LOOP
-for(let i=1;i<=5;i++){
-    console.log(i);
-}
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
 
 // WHILE LOOP
 
-let i=1;
-while(i<=5){
-    console.log(i)
-}
+// let i=1;
+// while(i<=5){
+//     console.log(i)
+// }
 
 // DO WHILE LOOP
 
-let i=1
+
+//INFINITE LOOP
+// let j=6;
+// do{
+//     console.log(j);
+// }while(j!=5);
+
+// NOT INFINITE LOOP
+let k=5;
+do{
+    console.log(k);
+}while(k!=5);
