c2a245891aba588e12b089dfe8dcdfe02215df7e
akshar51 <aksharparekh401@gmail.com>
2025-02-11 12:45:41 +0530

    done

--- Javascript Daily/Loop/script.js
+++ Javascript Daily/Loop/script.js
@@ -254,3 +254,55 @@ else{
 // 22) DIAMOND PATTERN USING NESTED LOOP
 
 
+// 23)SUM OF FIRST N PRIME NUMBER
+
+let sum = 0;
+let end = prompt("Ending number : ");
+
+for(let num = 2;num<=end;num++){
+    let isPrime = true;
+    for(let i=2;i<=Math.sqrt(num);i++){
+        if(num % i === 0){
+            isPrime = false;
+            break;
+        }
+    } 
+    if(isPrime){
+        sum+=num;
+    }
+}
+console.log(`Sum of prime number from 1 to ${end} : ${sum}`)
+
+// 26) PRINT ALL PRIME NUMBER IN RANGE
+
+let start = prompt("Starting number : ");
+let end = prompt("Ending number : ");
+
+for(let num = start;num<=end;num++){
+    let isPrime = true;
+    for(let i=2;i<=Math.sqrt(num);i++){
+        if(num % i === 0){
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime){
+        console.log(num)
+    }
+}
+
+// 27) PRINT HOLLOW SQUARE PATTERN USING LOOP
+
+let n = parseInt(prompt("Enter the size of the square:"));
+
+for (let i = 0; i < n; i++) { 
+    let row = "";
+    for (let j = 0; j < n; j++) { 
+        if (i === 0 || i === n - 1 || j === 0 || j === n - 1) {
+            row += "* ";
+        } else {
+            row += "  "; 
+        }
+    }
+    console.log(row); 
+}
--- Javascript Daily/Variable/script.js
+++ Javascript Daily/Variable/script.js
@@ -41,3 +41,51 @@
 // console.log(a%b);
 // console.log(a**3);// EXPONENTIAL-means power
 
+
+
+// let sum = 0;
+// let end = prompt("Ending number : ");
+
+// for(let num = 2;num<=end;num++){
+//     let isPrime = true;
+//     for(let i=2;i<=Math.sqrt(num);i++){
+//         if(num % i === 0){
+//             isPrime = false;
+//             break;
+//         }
+//     } 
+//     if(isPrime){
+//         sum+=num;
+//     }
+// }
+// console.log(`Sum of prime number from 1 to ${end} : ${sum}`)
+
+// let start = prompt("Starting number : ");
+// let end = prompt("Ending number : ");
+
+// for(let num = start;num<=end;num++){
+//     let isPrime = true;
+//     for(let i=2;i<=Math.sqrt(num);i++){
+//         if(num % i === 0){
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if(isPrime){
+//         console.log(num)
+//     }
+// }
+
+let n = parseInt(prompt("Enter the size of the square:"));
+
+for (let i = 0; i < n; i++) { // Loop for rows
+    let row = "";
+    for (let j = 0; j < n; j++) { // Loop for columns
+        if (i === 0 || i === n - 1 || j === 0 || j === n - 1) {
+            row += "* ";
+        } else {
+            row += "  "; // Print space for the hollow part
+        }
+    }
+    console.log(row); // Print each row
+}
