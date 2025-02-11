1f5d447cf87b60ab6d8ce362bc5d556dff0e2e07
akshar51 <aksharparekh401@gmail.com>
2025-02-11 17:18:19 +0530

    done

---
+++ Javascript Daily/Function/script.js
@@ -0,0 +1,22 @@
+// FUNCTION
+function functionName(){
+    console.log(` NORMAL FUNCTION`)
+}
+functionName();
+
+// FUNCTION - 2
+let print = function(){
+    console.log(`FUNCTION STORING IN VARIABLE`)
+}
+print();
+
+// FUNCTION - 3
+
+let print2 = ()=>{
+    console.log(`ARROW FUNCTION`)
+}
+print2();
+// FUNCTION - 4 
+(function(){
+    console.log(`IIEF FUNCTION`)
+})();
