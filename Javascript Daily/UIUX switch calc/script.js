869da8d80caf3de157f9fb76d5ba8a4bf0216698
akshar51 <aksharparekh401@gmail.com>
2025-02-13 13:19:28 +0530

    done

--- Javascript Daily/UIUX switch calc/script.js
+++ Javascript Daily/UIUX switch calc/script.js
@@ -1,14 +1,18 @@
 let num1;
 let num2;
-document.getElementById('result').classList.add('hidden')
+document.getElementById('result').classList.add('hidden');
+
+
 
 function printValue(){
     num1 = document.getElementById('num1').value;
     num2 = document.getElementById('num2').value;
-    document.getElementById('num1-value').innerHTML +=" "+num1
-    document.getElementById('num2-value').innerHTML +=" "+num2
-    document.getElementById('result').classList.remove('hidden')
-    document.getElementById('result').classList.add('active')
+
+    document.getElementById('num1-value').innerHTML +=" "+num1;
+    document.getElementById('num2-value').innerHTML +=" "+num2;
+
+    document.getElementById('result').classList.remove('hidden');
+    document.getElementById('result').classList.add('active');
 }
 
 function Operation(type){
@@ -17,10 +21,13 @@ function Operation(type){
             document.getElementById('ans').innerHTML = parseInt(num1) + parseInt(num2)
         break;
         case 'sub':
+            document.getElementById('ans').innerHTML = parseInt(num1) - parseInt(num2)
         break;
         case 'mult':
+            document.getElementById('ans').innerHTML = parseInt(num1) * parseInt(num2)
         break;
         case 'div':
+            document.getElementById('ans').innerHTML = parseInt(num1) / parseInt(num2)
         break;
     }
 }
