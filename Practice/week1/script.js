cd703a03c795289a3a1ef7c658461a0896342373
akshar51 <aksharparekh401@gmail.com>
2025-02-15 17:35:11 +0530

    done

--- Practice/week1/script.js
+++ Practice/week1/script.js
@@ -10,3 +10,34 @@
 // // console.log(p.innerHTML);
 
 // p.innerHTML += " with Akshar"
+
+let num1 = document.getElementById("num1");
+num1.addEventListener("click",function(){
+    document.getElementById("screen").innerHTML += 1;
+    num1=1;
+
+})
+
+let num2 = document.getElementById("num2");
+num2.addEventListener("click",function(){
+    document.getElementById("screen").innerHTML += 2;
+    num2=2;
+})
+
+let num3 = document.getElementById("num3");
+num3.addEventListener("click",function(){
+    document.getElementById("screen").innerHTML += 3;
+    num3=3;
+})
+
+let operation=(type)=>{
+    switch(type){
+        case 'add':
+            document.getElementById("screen").innerText += "+"
+            let newNum =document.getElementById("screen").innerHTML = parseInt(num1) + parseInt(num2);
+        break;
+        case 'sub':
+            document.getElementById("screen").innerHTML = parseInt(num1) - parseInt(num2);
+        break;
+    }
+}
