4d6cd0225cea32eda8ffc9e8ed33b6070ad0fbc9
akshar51 <aksharparekh401@gmail.com>
2025-02-15 16:09:34 +0530

    done

--- Practice/week1/script.js
+++ Practice/week1/script.js
@@ -1,5 +1,7 @@
-let main = document.querySelector(".main p")
+let main = document.querySelector("span")
 console.log(main);
 
 main.style.color = "red"
+main.style.backgroundColor = "green"
+main.style.display = 'block'
 
