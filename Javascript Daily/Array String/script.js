42613d115c3eee1cabc5b202675d35224f2711bd
akshar51 <aksharparekh401@gmail.com>
2025-02-17 17:26:12 +0530

    done

---
+++ Javascript Daily/Array String/script.js
@@ -0,0 +1,17 @@
+// POP (Delete last element)
+// let arr2 = [10,20,30,40,50];
+// arr2.pop();
+// console.log(arr2);
+
+// PUSH (Add element at last)
+// let arr = ["a","b","c","d"];
+// arr.push("e")
+// console.log(arr);
+
+
+// SHIFT ()
+
+let arr = ["a","b","c","d","e"];
+arr.shift();
+console.log(arr)
+
