c515ebe7bdddc6f45624f799a693a649b241fe4d
akshar51 <aksharparekh401@gmail.com>
2025-03-07 17:15:43 +0530

    done

--- Javascript Daily/Calculator/script.js
+++ Javascript Daily/Calculator/script.js
@@ -1,3 +1,25 @@
 let btn = document.querySelectorAll("button")
-let array = ['+','-','*','/','='];
+let display = document.querySelector("#display")
+let array = ['+','-','*','/'];
 
+btn.forEach((button)=>{
+    button.addEventListener("click",(e)=>{
+        let value = e.target.innerHTML
+        let lastStr = display.value.slice(0,-1)
+        console.log(lastStr)
+        if(value == '='){
+            if(array.includes()){
+                display.value = value
+            }
+            else{
+                display.value = eval(display.value)
+            }
+        }
+        else if(value == 'C'){
+            display.value = ""
+        }
+        else{
+            display.value += value;
+        }
+    })
+})
