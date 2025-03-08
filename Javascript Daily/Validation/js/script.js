f0578f3ac8acf407d4e906df3803861f16b97cad
akshar51 <aksharparekh401@gmail.com>
2025-03-08 15:39:30 +0530

    done

--- Javascript Daily/Validation/js/script.js
+++ Javascript Daily/Validation/js/script.js
@@ -2,4 +2,21 @@ let username = document.getElementById("username")
 let email = document.getElementById("email")
 let password = document.getElementById("password")
 let gender = document.querySelectorAll("input[name='gender']")
-let hobby = document.querySelectorAll("input=hobby['hobby']")
+let hobby = document.querySelectorAll("input[name='hobby']")
+
+let validate = ()=>{
+    
+    if(username.value.trim() === ""){
+        document.querySelector('.userError').classList.remove("d-none");
+    }
+    else{
+        document.querySelector('.userError').classList.add("d-none");
+    }
+
+    if(email.value.trim()===""){
+        document.querySelector('.emailError').classList.remove('d-none')
+    }
+    else{
+        document.querySelector('.emailError').classList.add('d-none')
+    }
+}
