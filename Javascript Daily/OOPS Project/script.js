e12e6d7607f52e20910ee631f2b48b27b596a1fb
akshar51 <aksharparekh401@gmail.com>
2025-03-19 16:42:38 +0530

    done

---
+++ Javascript Daily/OOPS Project/script.js
@@ -0,0 +1,23 @@
+class Student{
+    constructor(name,age,course){
+        this.e_name = '';
+        this.age = 0;
+        this.course = '';
+    }
+
+    set(name,age,course){
+        this.e_name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    get(){
+        console.log(`Name : ${this.e_name}`);
+        console.log(`Age : ${this.age}`);
+        console.log(`Course : ${this.course}`);
+    }
+}
+
+let obj = new Student();
+obj.set("Akshar",24,"Full Stack")
+obj.get()
