24ba4317d29d2734a694ead3946c065d7460fa79
akshar51 <aksharparekh401@gmail.com>
2025-03-27 17:39:43 +0530

    done

--- Javascript Daily/Json Form/script.js
+++ Javascript Daily/Json Form/script.js
@@ -2,7 +2,9 @@ let username = document.getElementById("username");
 let password = document.getElementById("password");
 let form = document.getElementById("form");
 let tableRow = document.querySelector("#table tbody");
+let updateBtn = document.getElementById("updateBtn")
 let dataStore = [];
+let editIdx = -1;
 username.focus();
 
 form.addEventListener("submit",(event)=>{
@@ -11,7 +13,17 @@ form.addEventListener("submit",(event)=>{
         username : username.value,
         password : password.value
     }
-    dataStore.push(obj);
+    if(editIdx == -1){
+        dataStore.push(obj);
+    }
+    else{
+        dataStore[editIdx]=obj;
+        editIdx = -1;
+        updateBtn.innerText = "Register";
+        updateBtn.classList.add("btn-success")
+        updateBtn.classList.remove("btn-primary")
+
+    }
     username.value = "";
     password.value = "";
     username.focus();
@@ -26,8 +38,25 @@ function displayData(){
         `<td>${idx+1}</td>
          <td>${data.username}</td>
          <td>${data.password}</td>
+         <td><button class="btn btn-warning px-3 me-2"  onclick="editData(${idx})">Edit</button>
+             <button class="btn btn-danger" onclick="deleteData(${idx})">Delete</button>
+         </td>
         `
         tableRow.append(row);
     })
 }
 displayData();
+
+let deleteData = (idx)=>{
+    dataStore.splice(idx,1);
+    displayData();
+}
+
+let editData = (idx)=>{
+    username.value = dataStore[idx].username;
+    password.value = dataStore[idx].password;
+    editIdx = idx;
+    updateBtn.innerText = "Update";
+    updateBtn.classList.remove("btn-success")
+    updateBtn.classList.add("btn-primary")
+}
