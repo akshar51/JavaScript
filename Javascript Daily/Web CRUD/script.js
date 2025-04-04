5df1bde63d0ddbcd0be4bb31317657ac11f63cc2
akshar51 <aksharparekh401@gmail.com>
2025-04-04 21:50:20 +0530

    done

--- Javascript Daily/Web CRUD/script.js
+++ Javascript Daily/Web CRUD/script.js
@@ -1,9 +1,11 @@
-let data = [];
+let data =  JSON.parse(localStorage.getItem("productData")) || [];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 let form = document.getElementById("form");
 let p_name = document.getElementById("p_name");
 let price = document.getElementById("price");
 let table = document.querySelector("#table tbody")
-p_name.focus()
+p_name.focus();
+
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
 
@@ -11,8 +13,9 @@ form.addEventListener("submit",(e)=>{
         p_name : p_name.value,
         price : price.value
     }
-
+    
     data.push(obj);
+    localStorage.setItem("productData",JSON.stringify(data))
     p_name.value = "";
     price.value="";
     p_name.focus()
@@ -20,7 +23,7 @@ form.addEventListener("submit",(e)=>{
 })
 
 const display=()=>{
-
+    table.innerHTML = "";
     data.map((val,idx)=>{
         let tr = document.createElement("tr")
         tr.innerHTML = 
@@ -30,14 +33,16 @@ const display=()=>{
         <td>${val.price}</td>
         <td></td>
         <td>
-            <button class="btn btn-success" onclick="addTocart()">Add to cart</button>
+            <button class="btn btn-success" onclick="addTocart(${idx})">Add to cart</button>
         </td>
         `
-        table.appendChild(tr)
+        table.append(tr)
     })
 }
 display()
 
-const addTocart = ()=>{
-
+const addTocart = (idx)=>{
+    const product = data[idx]; 
+    cart.push(product)
+    localStorage.setItem("cart",JSON.stringify(cart));
 }
