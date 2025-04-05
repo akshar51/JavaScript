e5372c5eb76e4ccd3dd992498ab64f5bc1c96065
akshar51 <aksharparekh401@gmail.com>
2025-04-05 17:54:25 +0530

    done

--- Javascript Daily/Web CRUD/script.js
+++ Javascript Daily/Web CRUD/script.js
@@ -4,6 +4,7 @@ let form = document.getElementById("form");
 let p_name = document.getElementById("p_name");
 let price = document.getElementById("price");
 let table = document.querySelector("#table tbody")
+let ulData = document.querySelector(".offcanvas-body ul")
 p_name.focus();
 
 form.addEventListener("submit",(e)=>{
@@ -34,6 +35,7 @@ const display=()=>{
         <td></td>
         <td>
             <button class="btn btn-success" onclick="addTocart(${idx})">Add to cart</button>
+            <button class="btn btn-danger" onclick="deleteData(${idx})">Delete</button>
         </td>
         `
         table.append(tr)
@@ -42,7 +44,35 @@ const display=()=>{
 display()
 
 const addTocart = (idx)=>{
+
     const product = data[idx]; 
     cart.push(product)
     localStorage.setItem("cart",JSON.stringify(cart));
+
+    ulData.innerHTML = "";
+    cart.map((val,idx)=>{
+        let li = document.createElement("li")
+        li.innerHTML = 
+        `
+        <td>${val.p_name}</td>
+        <td>${val.price}</td>
+        <td>
+            <button class="btn btn-danger" onclick="cartData(${idx})">Delete</button>
+        </td>
+        
+        `
+        ulData.append(li)
+    })
+}
+
+const deleteData = (idx)=>{
+    data.splice(idx,1);
+    display();
+    localStorage.setItem("productData",JSON.stringify(data))
+}
+
+const cartData = (idx)=>{
+    cart.splice(idx,1);
+    display();
+    localStorage.setItem("cart",JSON.stringify(cart))
 }
